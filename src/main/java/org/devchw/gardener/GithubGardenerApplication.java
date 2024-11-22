619d25462577df3b0dbb6833d234d35119135e73
DevCHW <ggoma003@naver.com>
2024-11-22 16:04:19 +0900

    first commit

---
+++ src/main/java/org/devchw/gardener/GithubGardenerApplication.java
@@ -0,0 +1,13 @@
+package org.devchw.gardener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGardenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubGardenerApplication.class, args);
+    }
+
+}
