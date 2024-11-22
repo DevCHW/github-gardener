f1f717e125b9c8fc045c07d05f4539c34ea25a89
DevCHW <ggoma003@naver.com>
2024-11-22 16:33:53 +0900

    ChatGPTClient 추가

---
+++ src/main/java/org/devchw/gardener/infra/clients/ChatGPTClient.java
@@ -0,0 +1,17 @@
+package org.devchw.gardener.infra.clients;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ChatGPTClient {
+
+    private final OpenAiChatModel openAiChatModel;
+
+    public String chat(String prompt) {
+        return openAiChatModel.call(prompt);
+    }
+
+}
---
+++ src/test/java/org/devchw/gardener/infra/clients/ChatGPTClientTest.java
@@ -0,0 +1,31 @@
+package org.devchw.gardener.infra.clients;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.ai.openai.OpenAiChatModel;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.*;
+
+class ChatGPTClientTest {
+    private OpenAiChatModel openAiChatModel = Mockito.mock(OpenAiChatModel.class);
+    private ChatGPTClient chatGPTClient = new ChatGPTClient(openAiChatModel);
+
+    @Test
+    @DisplayName("프롬프트를 받아 ChatGPT의 답변을 리턴한다.")
+    void chat() {
+        // given
+        String prompt = "hello GPT";
+        String answer = "hi";
+        given(openAiChatModel.call(anyString()))
+                .willReturn(answer);
+
+        // when
+        String result = chatGPTClient.chat(prompt);
+
+        // then
+        assertThat(result).isEqualTo(answer);
+    }
+}
